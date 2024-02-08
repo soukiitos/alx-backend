18f74192b0064b496f46ffdd68a8cfecfff87ade
Soukaina Lachheb <soukainalachheb93@gmail.com>
2024-02-09 00:35:19 +0100

    Update some fixes

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -1,23 +1,33 @@
 #!/usr/bin/env python3
 '''Get locale from request'''
-from flask import Flask, request
+from flask import Flask, render_template, request
 from flask_babel import Babel
-from routes.routes_2 import app_routes
-from config import Config
 
 
 app = Flask(__name__)
 babel = Babel(app)
 
 
-app.config.from_object(Config)
-app.register_blueprint(app_routes)
+class Config:
+    """Define Config"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object('2-app.Config')
 
 
 @babel.localeselector
 def get_locale() -> str:
     '''Define get_locale'''
-    return request.accept_languages.best_match(Config.LANGUAGES)
+    return request.accept_languages.best_match(['en', 'fr'])
+
+
+@app.route("/", methods=["GEt"], strict_slashes=False)
+def hello_world() -> str:
+    '''Define hello_world'''
+    return render_template('2-index.html')
 
 
 if __name__ == "__main__":
