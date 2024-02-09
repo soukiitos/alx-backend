784e9fa1bb615002162dc691801567e457fa9710
Soukaina Lachheb <soukainalachheb93@gmail.com>
2024-02-09 02:13:03 +0100

    Update some fixes

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -1,27 +1,36 @@
 #!/usr/bin/env python3
 '''Force locale with URL parameter'''
-from typing import Union
-from flask import Flask, request
+from flask import Flask, render_template, request
 from flask_babel import Babel
-from routes.routes_4 import app_routes
-from config import Config
 
 
 app = Flask(__name__)
 babel = Babel(app)
 
 
-app.config.from_object(Config)
-app.register_blueprint(app_routes)
+class Config:
+    """Define Config"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object('4-app.Config')
 
 
 @babel.localeselector
 def get_locale() -> Union[str, None]:
     '''Define get_locale'''
     locale = request.args.get('locale')
-    if locale and locale in Config.LANGUAGES:
+    if locale and locale in app.config['LANGUAGES']:
         return locale
-    return request.accept_languages.best_match(Config.LANGUAGES)
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route("/", methods=["GEt"], strict_slashes=False)
+def hello_world() -> str:
+    '''Define hello_world'''
+    return render_template('4-index.html')
 
 
 if __name__ == "__main__":
